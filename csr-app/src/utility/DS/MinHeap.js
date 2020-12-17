4fac6e4db3d02769a909643728c37fb212646d8d
luisalfonsopreciado <luisapreciado99@gmail.com>
2020-12-17 11:19:04 -0600

    Min Heap documented / code readability improvement

--- csr-app/src/utility/DS/MinHeap.js
+++ csr-app/src/utility/DS/MinHeap.js
@@ -1,68 +1,125 @@
+/* 
+  In a complete Binary Tree, For any ith node:
+  Arr[(i -1) / 2] returns its parent node.
+  Arr[(2 * i) + 1] returns its left child node.
+  Arr[(2 * i) + 2] returns its right child node.
+*/
+
+/**
+ * Min Heap implementation
+ */
 export class MinHeap {
+  /**
+   * function that return that value of two items
+   */
+  selector = null;
+
   constructor(selector) {
     this.items = [];
     this.selector = selector;
   }
-
+  /**
+   * Returns the Top most element from the Heap without removal
+   * @return {Object} obj
+   */
   peek() {
     return this.items[0];
   }
 
+  /**
+   * Insert an item into the Heap
+   * @param {Object} item
+   */
   push(item) {
     let i = this.items.length;
     this.items.push(item);
 
-    let parentIndex = Math.floor((i + 1) / 2 - 1);
-
-    if (parentIndex < 0) parentIndex = 0;
+    let parentIndex = this.getParentIndex(i);
 
     let parentVal = this.selector(this.items[parentIndex]);
     const pushedVal = this.selector(this.items[i]);
 
-    while (i > 0 && parentVal > pushedVal) {
-      parentIndex = Math.floor((i + 1) / 2 - 1);
-
+    while (parentVal > pushedVal) {
       this.swap(i, parentIndex);
 
       i = parentIndex;
+      parentIndex = this.getParentIndex(i);
 
-      parentVal = this.selector(
-        this.items[Math.max(Math.floor((i + 1) / 2 - 1), 0)]
-      );
+      parentVal = this.selector(this.items[parentIndex]);
     }
   }
 
+  /**
+   * Returns parent index of an input index.
+   * For index 0, the parent is 0.
+   * @param {number} index
+   * @returns {number} index - parent index
+   */
+  getParentIndex(index) {
+    return Math.max(Math.floor((index - 1) / 2), 0);
+  }
+
+  /**
+   * Returns the right child index
+   * @param {number} index
+   * @returns {number} index - right child index
+   */
+  getRightIndex(index) {
+    return 2 * index + 2;
+  }
+
+  /**
+   * Returns the left child index
+   * @param {number} index
+   * @returns {number} index - left child index
+   */
+  getLeftIndex(index) {
+    return 2 * index + 1;
+  }
+
+  /**
+   * Swap Two elements from the items list
+   * @param {number} i index i
+   * @param {number} j index j
+   */
   swap(i, j) {
     let t = this.items[i];
     this.items[i] = this.items[j];
     this.items[j] = t;
   }
 
+  /**
+   * Remove an Element from the Heap
+   * @returns {Object} obj - heap Element
+   */
   pop() {
     if (this.items.length <= 1) return this.items.pop(); //If it is the last element pop it
     const ret = this.items[0]; // What we will return
-    let temp = this.items.pop();
+    let temp = this.items.pop(); // Remove first item from items list
     this.items[0] = temp; // Place last element in array at front
 
     let i = 0; // We adjust heap from top to down
 
     while (true) {
-      let rightChildIndex = (i + 1) * 2;
-      let leftChildIndex = (i + 1) * 2 - 1;
+      let rightChildIndex = this.getRightIndex(i);
+      let leftChildIndex = this.getLeftIndex(i);
+      // Hold the index of the lowest child
       let lowest = rightChildIndex;
 
-      if (
-        leftChildIndex >= this.items.length &&
-        rightChildIndex >= this.items.length
-      )
-        break;
-      if (leftChildIndex >= this.items.length) lowest = rightChildIndex;
-      if (rightChildIndex >= this.items.length) lowest = leftChildIndex;
+      let leftIndexOB = leftChildIndex >= this.items.length;
+      let rightIndexOB = rightChildIndex >= this.items.length;
 
-      if (
-        !(leftChildIndex >= this.items.length) &&
-        !(rightChildIndex >= this.items.length)
-      ) {
+      // If both children are non-existent, we have finished setting up the heap
+      if (leftIndexOB && rightIndexOB) break;
+
+      // If left child is OB set lowest equal to the rightChildIndex
+      if (leftIndexOB) lowest = rightChildIndex;
+      // If right child is OB set lowest equal to the leftChildIndex
+      if (rightIndexOB) lowest = leftChildIndex;
+
+      let bothChildrenInBounds = !leftIndexOB && !rightIndexOB;
+
+      if (bothChildrenInBounds) {
         lowest =
           this.selector(this.items[rightChildIndex]) <
           this.selector(this.items[leftChildIndex])
@@ -74,28 +131,42 @@ export class MinHeap {
       if (this.selector(this.items[i]) > this.selector(this.items[lowest])) {
         this.swap(i, lowest);
         i = lowest;
-      } else break; // We have finished setting up the heap
+      } else {
+        break;
+      } // We have finished setting up the heap
     }
 
     // Return topmost element
     return ret;
   }
 
+  /**
+   * Return true if the heap contains the input item
+   * @param {Object} item
+   */
   contains(item) {
     return this.items.includes(item);
   }
 
+  /**
+   * Returns true if the heap is empty
+   * @returns {boolean} boolean
+   */
   isEmpty() {
     return this.items.length === 0;
   }
 
+  /**
+   * Remove an Item from the heap
+   * @param {Object} item
+   */
   delete(item) {
     let i = this.items.indexOf(item);
     // heapify
     this.items[i] = this.items.pop();
     while (true) {
       let lowest =
-        this.selector(this.items[(i + 1) * 2]) <
+        this.selector(this.items[this.getRightIndex(i)]) <
         this.selector(this.items[(i + 1) * 2 - 1])
           ? (i + 1) * 2
           : (i + 1) * 2 - 1;
@@ -108,12 +179,19 @@ export class MinHeap {
     }
   }
 
+  /**
+   * Print the contents of the Heap
+   */
   print() {
     for (let i = 0; i < this.items.length; i++) {
       console.log(this.items[i]);
     }
   }
 
+  /**
+   * Heapify
+   * @param {Object[]} arr
+   */
   heapify(arr) {
     for (let i = 0; i < arr.length; i++) {
       this.push(arr[i]);
