cb99cf050476ffcf4f7dbe1cd2e5b85e5c0da8da
luisalfonsopreciado <luisapreciado99@gmail.com>
2020-12-21 16:38:42 -0600

    recursive division maze generator documented

--- csr-app/src/utility/MazeGenerators/RecursiveDivision.js
+++ csr-app/src/utility/MazeGenerators/RecursiveDivision.js
@@ -1,16 +1,24 @@
 import { drawContourWalls } from "./Contour";
-
-// Constants to avoid typo's
-const HORIZONTAL = "horizontal";
-const VERTICAL = "vertical";
+import * as cts from "../constants";
 
 export const recursiveDivision = (grid, type) => {
   drawContourWalls(grid, type);
-  const width = grid[0].length - 2; // Subtract 2 since we drew a contour
+  // Subtract 2 accounting for drawn contour
+  const width = grid[0].length - 2;
   const height = grid.length - 2;
-  let prohibitedCells = []; // Walls that clot the maze
+  // An array that contains the nodes that would clot the maze
+  let prohibitedCells = [];
 
-  divide(1,1,width, height, chooseOrientation(width, height), prohibitedCells, grid, type)
+  divide(
+    1,
+    1,
+    width,
+    height,
+    chooseOrientation(width, height),
+    prohibitedCells,
+    grid,
+    type
+  );
 
   // Remove prohibited cells so that all of the board is reachable
   for (let i in prohibitedCells) {
@@ -20,6 +28,19 @@ export const recursiveDivision = (grid, type) => {
   }
 };
 
+/**
+ * Perform a single division on an input grid.
+ *
+ * @param {number} row - top left corner row
+ * @param {number} col - top left corner column
+ * @param {number} width - width of the grid (inclusive)
+ * @param {number} height - height of the grid (inclusive)
+ * @param {string} orientation - the orientation to split the grid
+ * @param {Node[]} prohibited - array that holds the cells that are restricted,
+ * used to avoid clots.
+ * @param {Node[][]} grid - inputGrid
+ * @param {*} type - the type of division (Walls or Weight)
+ */
 const divide = (
   row,
   col,
@@ -30,57 +51,83 @@ const divide = (
   grid,
   type
 ) => {
-  if (width <= 2 || height <= 2) return; //Maybe have a function that paints?
+  if (width <= 2 || height <= 2) return;
+
   // Is the wall to be drawn horizontal?
-  const horizontal = orientation === HORIZONTAL;
+  const isCutHorizontal = orientation === cts.HORIZONTAL;
 
   // Where will the wall be drawn from?
-  let whereCol = col + (horizontal ? 0 : randBetween(2, width - 2)); // Rand number between the col and width
-  let whereRow = row + (horizontal ? randBetween(2, height - 2) : 0); // Rand number between the row and height
+  let whereCol = col + (isCutHorizontal ? 0 : randBetween(2, width - 2)); // Rand number between the col and width
+  let whereRow = row + (isCutHorizontal ? randBetween(2, height - 2) : 0); // Rand number between the row and height
 
   // Where will the passage through the wall exist?
-  const passCol = whereCol + (horizontal ? randBetween(0, width - 1) : 0); // Rand number between the col and width
+  const passCol = whereCol + (isCutHorizontal ? randBetween(0, width - 1) : 0); // Rand number between the col and width
 
-  const passRow = whereRow + (horizontal ? 0 : randBetween(0, height - 1)); // Rand number between the row and height
+  const passRow = whereRow + (isCutHorizontal ? 0 : randBetween(0, height - 1)); // Rand number between the row and height
 
   // How long will the wall be?
-  const length = horizontal ? width : height;
+  const length = isCutHorizontal ? width : height;
 
   // Draw the walls
-  drawWall(whereRow, whereCol, horizontal, length, grid, type);
-  updateProhibitedCells(passRow, passCol, horizontal, prohibited);
+  drawWall(whereRow, whereCol, isCutHorizontal, length, grid, type);
+  updateProhibitedCells(passRow, passCol, isCutHorizontal, prohibited);
 
   // get first recursive call data
-  let newwidth = horizontal ? width : whereCol - col;
-  let newheight = horizontal ? whereRow - row : height;
-  let neworientation = chooseOrientation(newwidth, newheight);
-  
-  divide(row, col, newwidth, newheight, neworientation, prohibited,grid, type);
-  
+  let newWidth = isCutHorizontal ? width : whereCol - col;
+  let newHeight = isCutHorizontal ? whereRow - row : height;
+  let newOrientation = chooseOrientation(newWidth, newHeight);
+
+  divide(row, col, newWidth, newHeight, newOrientation, prohibited, grid, type);
+
   // get second recursive call data
-  let newCol = horizontal ? col : whereCol + 1;
-  let newRow = horizontal ? whereRow + 1 : row;
+  let newCol = isCutHorizontal ? col : whereCol + 1;
+  let newRow = isCutHorizontal ? whereRow + 1 : row;
 
-  newwidth = horizontal ? width : col + width - whereCol - 1;
-  newheight = horizontal ? row + height - whereRow - 1: height;
-  neworientation = chooseOrientation(newwidth, newheight);
+  newWidth = isCutHorizontal ? width : col + width - whereCol - 1;
+  newHeight = isCutHorizontal ? row + height - whereRow - 1 : height;
+  newOrientation = chooseOrientation(newWidth, newHeight);
 
-  divide(newRow, newCol, newwidth, newheight, neworientation, prohibited,grid, type);
+  divide(
+    newRow,
+    newCol,
+    newWidth,
+    newHeight,
+    newOrientation,
+    prohibited,
+    grid,
+    type
+  );
 };
 
-const drawWall = (startRow, startCol, horizontal, length, grid,type) => {
+/**
+ * Draw a wall on the grid
+ * @param {number} startRow
+ * @param {number} startCol
+ * @param {boolean} isCutHorizontal
+ * @param {number} length - The length of the wall
+ * @param {Node[][]} grid - Reference to the Node Grid
+ * @param {string} type - Type of impediment to draw: Wall or Weight
+ */
+const drawWall = (startRow, startCol, isCutHorizontal, length, grid, type) => {
   for (let i = 0; i < length; i++) {
-    const row = startRow + (horizontal ? 0 : i);
-    const col = startCol + (horizontal ? i : 0);
+    const row = startRow + (isCutHorizontal ? 0 : i);
+    const col = startCol + (isCutHorizontal ? i : 0);
 
     const cell = grid[row][col];
     if (!cell.isKeyValue()) cell.add([type]);
   }
 };
 
+/**
+ * Helper Method that determines if the cut should be horizontal or vertical
+ * based on the input dimensions.
+ * If the width is less than the height return horizontal else vertical.
+ * @param {number} width
+ * @param {number} height
+ */
 const chooseOrientation = (width, height) => {
-  if (width < height) return HORIZONTAL;
-  return VERTICAL;
+  if (width < height) return cts.HORIZONTAL;
+  return cts.VERTICAL;
 };
 
 const updateProhibitedCells = (row, col, horizontal, prohibited) => {
@@ -94,7 +141,12 @@ const updateProhibitedCells = (row, col, horizontal, prohibited) => {
   }
 };
 
-// stack overflow https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+/**
+ * Generate a random integer between a range [min, max] (inclusive).
+ * Extracted from : https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+ * @param {number} min must be less than max
+ * @param {number} max must be greater than min
+ */
 function randBetween(min, max) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
--- csr-app/src/utility/constants.js
+++ csr-app/src/utility/constants.js
@@ -1,3 +1,4 @@
+/* Algorithm Constants */
 export const DIJKSTRA = "Dijkstra"
 export const ASTAR = "A*"
 export const GREEDY_BFS = "Greedy Best First Search"
@@ -10,10 +11,12 @@ export const FLOYD_WARSHALL = "Floyd-Warshall"
 export const BELLMAN_FORD = "Bellman-Ford"
 export const BIDIRECTIONAL_BFS = "Bidirectional BFS"
 
+/* Maze Generator Constants */
 export const RAND_MAZE = "Random Maze"
 export const RECURSIVE_DIVISON = "Recursive Division"
 export const DRAW_COUNTOUR = "Contour Walls"
 
+/* Board State Constants */
 export const ADDING_WEIGHT = "Adding Weight"
 export const MOVING_TARGET = "Moving Target"
 export const MOVING_START = "Moving Start"
@@ -23,6 +26,11 @@ export const DELETING = "Deleting"
 export const PLACING_WALLS = "Placing Walls"
 export const VISUALIZING = "Visualizing"
 
+/* Board Element Constants */
+export const WALL = "Wall"
+export const WEIGHT = "Weight"
+
+/* Algorithm Introduction Constants */
 export const ALG_TITLE = {}
 ALG_TITLE[DIJKSTRA] = "Dijkstra's Algorithm is a weighted algorithm that guarantees the shortest path"
 ALG_TITLE[ASTAR] = "A* Algorithm is a weighted algorithm that guarantees the shortest path"
@@ -32,3 +40,7 @@ ALG_TITLE[BFS] = "BFS is an un-weighted algorithm, it guarantees the shortest pa
 ALG_TITLE[DFS] = "DFS is an un-weighted algorithm with the purpose of traversing a graph, shortest path/distance is NOT guaranteed"
 ALG_TITLE[PRIMS] = "Prim's algorithm is used to find the Minimum Spanning Tree of a weighted Graph, shortest path/distance is NOT guaranteed"
 ALG_TITLE[KRUSKAL] = "Kruskal's algorithm is used to find the Minimum Spanning Tree of a weighted Graph, shortest path/distance is NOT guaranteed"
+
+/* Miscellaneous */
+export const HORIZONTAL = "horizontal";
+export const VERTICAL = "vertical";
